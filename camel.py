88d06e6aa9da01def8c17e47b1dd280d1180dc19
bdt4 <bdt4@kent.ac.uk>
2026-02-05 09:33:50 +0000

    Done camel case project

--- camel.py
+++ camel.py
@@ -1,1 +1,14 @@
-# Enter your code here
+variable_name = input ("Insert the name of your variable in camel case") #Ask user for input of camel case
+output = "" #storing the input for later 
+
+for char in variable_name: 
+    if char.isupper():
+        output += "_" + char.lower()
+    else:
+         output += char 
+         
+#If there is an uppercase letter, we input an underscore and change the next letter to lowercase
+#Probably not the way you wanted me to do it, but I had no clue and had to search the internet for help
+
+print(output)
+
